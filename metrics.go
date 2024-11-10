d373d656c07636e34acbdebd82228b08cba99cda
Angel Ramirez <angelra@gmail.com>
2024-11-10 15:53:37 -0800

    add metrics endpoint

---
+++ metrics.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+		cfg.fileServerHits.Add(1)
+	})
+}
--- server.go
+++ server.go
@@ -3,26 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
 type Server struct {
 	Handler *http.ServeMux
 	Addr    string
+	Config  apiConfig
 }
 
 const (
-	serverRootPath = http.Dir("./src")
+	serverRootPath    = http.Dir("./src")
+	contentType       = "Content-Type"
+	headerContentType = "text/plain; charset=utf-8"
 )
 
 func createServer(port string) *Server {
-	return &Server{http.NewServeMux(), ":" + port}
+	return &Server{http.NewServeMux(), ":" + port, apiConfig{fileServerHits: atomic.Int32{}}}
 }
 
 func startServer() {
 	server := createServer("8080")
 
-	server.Handler.Handle("/app/", http.StripPrefix("/app/", http.FileServer(serverRootPath)))
+	server.Handler.Handle("/app/", http.StripPrefix("/app/", server.Config.middlewareMetricsInc(http.FileServer(serverRootPath))))
 	server.Handler.HandleFunc("/healthz", handleReadiness)
+	server.Handler.HandleFunc("/metrics", server.Config.handlerMetrics)
 
 	fmt.Printf("🐣 Chirping on http://localhost%s\n", server.Addr)
 	err := http.ListenAndServe(server.Addr, server.Handler)
@@ -34,7 +40,14 @@ func startServer() {
 }
 
 func handleReadiness(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add(contentType, headerContentType)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add(contentType, headerContentType)
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileServerHits.Load()
+	w.Write([]byte(strconv.Itoa(int(hits))))
 }
